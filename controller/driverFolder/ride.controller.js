eb9737e893ce0487482e6ae73160a77e5dcf7403
vanshikarathore16 <vanshikarathore57@gmail.com>
2025-12-01 19:01:34 +0530

    add ride api

--- controller/driverFolder/ride.controller.js
+++ controller/driverFolder/ride.controller.js
@@ -16,6 +16,43 @@ export const createRide = async (req, res) => {
     if (!rideType) return res.status(400).json({ error: "Invalid Ride Type" });
     let fareData = {};
     let finalPrice = null;
+    if (!customerOfferPrice) {
+      const basePrice = rideType.basePrice;
+      const distancePrice = distanceKm * rideType.pricePerKm;
+      const timePrice = durationMin * rideType.pricePerMin;
+      const subtotal = basePrice + distancePrice + timePrice;
+      fareData = {
+        basePrice,
+        distancePrice,
+        timePrice,
+        surgeMultiplier: 1,
+        taxes: subtotal * 0.05, // example 5% GST
+        platformFee: subtotal * 0.1, // 10% company fee
+        discount: 0,
+        total: subtotal + subtotal * 0.05 + subtotal * 0.1,
+      };
+      finalPrice = fareData.total;
+    }
+    const ride = await Ride.create({
+      rideCode: "RIDE" + Date.now(),
+      customerId,
+      rideTypeId,
+      pickup,
+      drop,
+      distanceKm,
+      durationMin,
+      // Bidding case → store offer price
+      customerOfferPrice: customerOfferPrice || null,
+      // Non-bidding case → store calculated fare
+      fare: fareData,
+      finalAgreedPrice: finalPrice,
+      status: "requested",
+    });
+
+    return res.status(201).json({
+      message: "Ride request created successfully",
+      ride,
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal Server Error" });
